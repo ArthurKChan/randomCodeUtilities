639fb3d5d864b932c5d239c8cb2e191fd3c00d37
Arfurboy <arfurboy@gmail.com>
2015-02-11 22:57:55 -0800

    (refactor) gives insertion sort a comparator option

--- insertionSort.js
+++ insertionSort.js
@@ -1,12 +1,16 @@
-function insertionSort(list){
+function insertionSort(list, comparator){
+  comparator = comparator || defaultComparator;
   for(var i=1; i<list.length; i++){
     for(var j=0; j<i; j++){
-      if (list[i] < list[j]) {
+      if (comparator(list[i],list[j]) < 0) {
         var item = list.splice(i,1);
         list.splice(j, 0, item[0]);
       }
     }
-    console.log(list)
   }
   return list;
+};
+
+function defaultComparator(a, b){
+  return a-b;
 };
