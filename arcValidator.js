ad766fc5653fd3d42086c3686ca385a7c361ac9d
Arfurboy <arfurboy@gmail.com>
2015-02-16 00:26:23 -0800

    initiate arcValidator

---
+++ arcValidator.js
@@ -0,0 +1,7 @@
+// function arcValidator
+// input: start
+//        end
+//        arcs (array of {start:char, end:char})
+// output:
+//          true -> there is a path from start to end (or start == end)
+//          false -> there is no path from start to end
