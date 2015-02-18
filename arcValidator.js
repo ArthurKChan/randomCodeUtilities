3d04924b682254d90c518616d66246d60f47ba37
Arfurboy <arfurboy@gmail.com>
2015-02-17 18:08:53 -0800

    adds some arc samples to test later

--- arcValidator.js
+++ arcValidator.js
@@ -5,3 +5,12 @@
 // output:
 //          true -> there is a path from start to end (or start == end)
 //          false -> there is no path from start to end
+
+var sampleArcs = {
+  "1": [{s:"a",e:"b"}],
+  "2": [{s:"a",e:"b"},{s:"b",e:"c"}],
+  "3": [{s:"a",e:"b"},{s:"b",e:"c"},{s:"c",e:"a"}],
+  "4": [{s:"a",e:"b"},{s:"b",e:"c"},{s:"c",e:"a"},{s:"c",e:"d"},{s:"d",e:"e"}],
+  "5": [{s:"a",e:"b"},{s:"b",e:"c"},{s:"c",e:"a"},{s:"c",e:"d"},{s:"d",e:"e"},{s:"e",e:"f"},{s:"f",e:"g"},{s:"g",e:"h"},{s:"h",e:"d"},{s:"h",e:"i"}]
+};
+
