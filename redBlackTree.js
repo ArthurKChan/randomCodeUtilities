776b206ec38d1e601e14e7803f3613f4387ffe34
Arfurboy <arfurboy@gmail.com>
2015-02-23 18:05:38 -0800

    init redBlackTree

---
+++ redBlackTree.js
@@ -0,0 +1,16 @@
+// create node
+function Node(value){
+  this.value = value;
+  this.color = "red";
+  this.children = [null, null];
+};
+// Tree Insertion
+function RedBlackTree(value){
+  if (!value) {
+    
+  }
+};
+// Tree Removal
+
+// Tree Tests
+
