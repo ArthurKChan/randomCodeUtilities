259bd525401978b951efa67b90d94d2770c250b2
Arfurboy <arfurboy@gmail.com>
2015-02-27 15:23:48 -0800

    adds 2nd pass for compiler

--- threePassCompiler.js
+++ threePassCompiler.js
@@ -111,6 +111,25 @@ Compiler.prototype.pass1 = function (program) {
 };
 
 Compiler.prototype.pass2 = function (ast) {
+  if (ast.op === "imm") {
+    return ast;
+  }
+  if (ast.op === "arg") {
+    return ast;
+  }
+  var a = this.pass2(ast.a), b = this.pass2(ast.b);
+  if (ast.op === "*") {
+    return a.op === "imm" && b.op === "imm" ? {"op":"imm","n": a.n * b.n} : {"op":"*","a":a,"b":b};
+  }
+  if (ast.op === "/") {
+    return a.op === "imm" && b.op === "imm" ? {"op":"imm","n": a.n / b.n} : {"op":"/","a":a,"b":b};
+  }
+  if (ast.op === "+") {
+    return a.op === "imm" && b.op === "imm" ? {"op":"imm","n": a.n + b.n} : {"op":"+","a":a,"b":b};
+  }
+  if (ast.op === "-") {
+    return a.op === "imm" && b.op === "imm" ? {"op":"imm","n": a.n - b.n} : {"op":"-","a":a,"b":b};
+  }
   // return AST with constant expressions reduced
 };
 
@@ -119,5 +138,8 @@ Compiler.prototype.pass3 = function (ast) {
 };
 
 var c = new Compiler;
-console.log(JSON.stringify(c.pass1("[ x y z ] ( 2*3*x + 5*y - 3*z ) / (1 + 3 + 2*2)")));
+// console.log(JSON.stringify(c.pass1("[ x y z ] ( 2*3*x + 5*y - 3*z ) / (1 + 3 + 2*2)")));
+console.log(JSON.stringify(c.pass2(c.pass1("[ x y z ] ( 2*3*x + 5*y - 3*z ) / (1 + 3 + 2*2)"))));
+
+// {"op":"/","a":{"op":"-","a":{"op":"+","a":{"op":"*","a":{"op":"imm","n":6},"b":{"op":"arg","n":0}},"b":{"op":"*","a":{"op":"imm","n":5},"b":{"op":"arg","n":1}}},"b":{"op":"*","a":{"op":"imm","n":3},"b":{"op":"arg","n":2}}},"b":{"op":"imm","n":8}}
 
