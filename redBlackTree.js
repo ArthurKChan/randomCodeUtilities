4037a45e91efaf518d1707f61438f1e546a455de
Arfurboy <arfurboy@gmail.com>
2015-02-26 22:50:11 -0800

    update

--- redBlackTree.js
+++ redBlackTree.js
@@ -11,15 +11,14 @@ var NullNode = function(){
 };
 
 var RedBlackTree = function(){
-  this.value = null;
   this.color = "black";
+  this.value = null;
 };
 
 // Tree Insertion
 RedBlackTree.prototype.insert = function(value){
   if (this.value === null) {
-    this.value = value;
-    this.color = "red";
+    this = new RedBlackTree(value);
     // enforce rules
   }
   if (this.value < value) {
