cd4cc42efcb4cab9a46f71d312d0ab6d37ef08b3
Arfurboy <arfurboy@gmail.com>
2015-02-26 22:30:57 -0800

    inits node and null node classes

--- redBlackTree.js
+++ redBlackTree.js
@@ -1,19 +1,36 @@
-// create node
-function RedBlackNode(value){
-  if (!value) {
-    // do nothing
-  }
+var RedBlackNode = function(value){
   this.value = value;
   this.color = "red";
-  this.children = [null, null];
+  this.left = new NullNode;
+  this.right =  new NullNode;
 };
-// Tree Insertion
-RedBlackNode.prototype.insert = function(value){
 
+var NullNode = function(){
+  this.color = "black";
+  this.value = null;
+};
+
+var RedBlackTree = function(){
+  this.value = null;
+  this.color = "black";
+};
+
+// Tree Insertion
+RedBlackTree.prototype.insert = function(value){
+  if (this.value === null) {
+    this.value = value;
+    this.color = "red";
+    // enforce rules
+  }
+  if (this.value < value) {
+    this.right.insert(value);
+  }
+  if (this.value > value) {
+    this.left.insert(value);
+  }
 };
 // Tree Removal
-RedBlackNode.prototype.remove = function(value){
-
+RedBlackTree.prototype.remove = function(value){
+  //... do later
 };
-// Tree Tests
 
